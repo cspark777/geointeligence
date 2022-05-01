b5e8e5b1afbc0f8315df6d7d6eb58a3bd99afbb0
cspark777 <cspark777@hotmail.com>
2022-05-01 08:09:38 +0800

    fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
+    path('',views.login,name='index'),
 ]
